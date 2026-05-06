82d6b26d121174d3d1b058f064822d037ba1b8df
umayer10-AI <mdumayer10@gmail.com>
2026-05-06 15:47:15 +0600

    add

--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const g = async () => {
@@ -12,6 +13,18 @@ const page = async () => {
   return (
     <div>
         <h2>Data: {data.length}</h2>
+        <Link href={'/about'} className='bg-blue-500 font-bold py-2 px-4 rounded-2xl'>Back</Link>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                {
+                    data.map(v => (
+                        <div key={v.id} className='border border-amber-50 p-3 rounded-2xl'>
+                            <h2 className='text-2xl font-bold'>{v.id}</h2>
+                            <h2 className='text-2xl font-bold text-green-500'>{v.name}</h2>
+                            <h2 className='text-2xl font-bold text-yellow-500'>{v.email}</h2>
+                        </div>
+                    ))
+                }
+            </div>
     </div>
   );
 };
