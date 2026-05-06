153f88824e0634b4e2fdb596537602786d5016a2
umayer10-AI <mdumayer10@gmail.com>
2026-05-06 15:51:46 +0600

    add

---
+++ app/src/app/about/page.jsx
@@ -0,0 +1,66 @@
+"use client";
+import {FloppyDisk} from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  FieldGroup,
+  Fieldset,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+// import { redirect } from "next/navigation";
+import React from 'react';
+
+const page = () => {
+
+    const a = async (v) => {
+        console.log(v)
+    }
+
+    return (
+        <div className='flex justify-center'>
+            <Form className="w-full max-w-96" onSubmit={handleSubmit(a)}>
+      <Fieldset>
+        <Fieldset.Legend>Profile Settings</Fieldset.Legend>
+        <Description>Update your profile information.</Description>
+        <FieldGroup>
+          <TextField
+            isRequired
+            name="name"
+            validate={(value) => {
+              if (value.length < 3) {
+                return "Name must be at least 3 characters";
+              }
+              return null;
+            }}
+          >
+            <Label>Name</Label>
+            <Input placeholder="John Doe" {...register("name", { required: true })}/>
+            <FieldError />
+          </TextField>
+          <TextField isRequired name="email" type="email">
+            <Label>Email</Label>
+            <Input placeholder="john@example.com" {...register("email", { required: true })}/>
+            <FieldError />
+          </TextField>
+          
+        </FieldGroup>
+        <Fieldset.Actions>
+          <Button type="submit">
+            <FloppyDisk />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Cancel
+          </Button>
+        </Fieldset.Actions>
+      </Fieldset>
+    </Form>
+        </div>
+    );
+};
+
+export default page;
